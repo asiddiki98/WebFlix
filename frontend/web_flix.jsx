1484ceff208e407b815a9a6de569087412a73ecb
asiddiki98 <azimsardines@gmail.com>
2021-01-20 09:47:41 -0500

    switching off main

---
+++ frontend/web_flix.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+    ReactDOM.render(<h1 className="header">WebFlix</h1>, root);
+});
